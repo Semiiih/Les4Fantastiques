7440f7bb56b286790483944768cad99a341ec347
Semih Cakici <semih.cakici34@gmail.com>
2024-12-10 10:40:23 +0100

    api dylan, style base

--- Les4Fantastiques/Les4Fantastiques/views/personnage.py
+++ Les4Fantastiques/Les4Fantastiques/views/personnage.py
@@ -1,62 +1,7 @@
 from django.views.generic import TemplateView
-import hashlib
-import requests
-from datetime import datetime
-
-
-
-from django.views.generic import TemplateView
-import json
-import os
-import os
-import json
-from django.conf import settings
-
-
-
-
-from django.views.generic import TemplateView
-import requests
-import hashlib
-import time
-
-
-from marvel import Marvel
-from marvel.exceptions import MarvelException
-
-
-# Clés Marvel
-# PUBLIC_KEY = "819bf36e600faa8dba5d37abc18ce47b"
-# PRIVATE_KEY = "705034aa77b79b0b158adb0473d6cd07a68a5c5d"
-
-# m = Marvel(PUBLIC_KEY, PRIVATE_KEY)
-# characters = m.characters  
-
-
-# class PersonnageView(TemplateView):
-#     template_name = "personnage.html"
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-
-#         try:
-#             all_characters = characters.all(limit=20)
-#             context['characters'] = all_characters['data']['results'] 
-
-#         except MarvelException as e:
-#             context['error'] = f"Erreur Marvel API : {str(e)}"
-
-#         except Exception as e:
-#             context['error'] = f"Erreur inattendue : {str(e)}"
-
-#         return context
-
-from marvel import Marvel
-from django.views.generic import TemplateView
 import requests
 
-# API_URL = "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=819bf36e600faa8dba5d37abc18ce47b&hash=a18d492c1d8426fc7647581293cb145e"
-API_URL = "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=eb79c3a5b07c45555c5af6c1ca4be64e&hash=a18d492c1d8426fc7647581293cb145e"
+API_URL = "https://dylanprof.pythonanywhere.com/characters/"
 
 class PersonnageView(TemplateView):
     template_name = "personnage.html"
@@ -68,9 +13,16 @@ class PersonnageView(TemplateView):
             response = requests.get(API_URL, verify=False)  
             response.raise_for_status()  
             data = response.json()  
-            print(data, 'iiiviiiiiii')
+            
             characters = data.get('data', {}).get('results', [])
-            context['characters'] = characters
+            valid_characters = []
+
+            for character in characters:
+                thumbnail = character.get('thumbnail', {})
+                if thumbnail.get('path') and thumbnail.get('extension') and 'image_not_available' not in thumbnail.get('path'):
+                    valid_characters.append(character)
+
+            context['characters'] = valid_characters
 
         except requests.exceptions.SSLError as ssl_error:
             context['error'] = "Erreur SSL lors de l'appel à l'API Marvel. Vérifiez votre connexion ou les certificats."
@@ -83,60 +35,3 @@ class PersonnageView(TemplateView):
             context['error'] = "Impossible de récupérer les données des personnages Marvel."
 
         return context
-
-
-
-
-# class PersonnageView(TemplateView):
-#     template_name = "personnage.html"
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-
-#         json_path = os.path.join(settings.BASE_DIR, 'Les4Fantastiques/views/appelCharacterMarvel.json')
-
-#         try:
-#             with open(json_path, 'r') as file:
-#                 data = json.load(file)
-#                 characters = data.get('data', {}).get('results', [])
-#                 context['characters'] = characters
-#         except FileNotFoundError:
-#             context['error'] = 'Le fichier JSON n\'a pas été trouvé.'
-#         except json.JSONDecodeError:
-#             context['error'] = 'Erreur lors de la lecture du fichier JSON.'
-
-#         return context
-
-
-
-# class PersonnageView(TemplateView):
-#     template_name = "personnage.html"
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-
-#         public_key = '819bf36e600faa8dba5d37abc18ce47b'
-#         private_key = '705034aa77b79b0b158adb0473d6cd07a68a5c5d'
-#         timestamp = str(int(datetime.timestamp(datetime.now())))
-#         hash_value = hashlib.md5(f'{timestamp}{private_key}{public_key}'.encode()).hexdigest()
-
-#         print(f"Hash Value: {hash_value}")
-#         print(f"timestamp: {timestamp}")
-
-#         url = "https://gateway.marvel.com:443/v1/public/characters"
-#         params = {
-#             'ts': timestamp,
-#             'apikey': public_key,
-#             'hash': hash_value,
-#             'limit': 20  
-#         }
-#         try:
-#             response = requests.get(url, params=params)
-#             response.raise_for_status()
-#             data = response.json()
-#             characters = data.get('data', {}).get('results', [])
-#             context['characters'] = characters
-#         except requests.exceptions.RequestException as e:
-#             context['error'] = str(e)
-
-#         return context
--- Les4Fantastiques/Les4Fantastiques/views/serie.py
+++ Les4Fantastiques/Les4Fantastiques/views/serie.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
-import json
-import os
-from django.conf import settings
+import requests
+
+API_URL = "https://dylanprof.pythonanywhere.com/series/"
 
 class SerieView(TemplateView):
     template_name = "serie.html"
@@ -9,16 +9,27 @@ class SerieView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        json_path = os.path.join(settings.BASE_DIR, 'Les4Fantastiques/views/appelSerieMarvel.json')
-
         try:
-            with open(json_path, 'r') as file:
-                data = json.load(file)
-                series = data.get('data', {}).get('results', [])
-                context['series'] = series
-        except FileNotFoundError:
-            context['error'] = 'Le fichier JSON n\'a pas été trouvé.'
-        except json.JSONDecodeError:
-            context['error'] = 'Erreur lors de la lecture du fichier JSON.'
+            response = requests.get(API_URL)
+            response.raise_for_status()
+            
+            data = response.json()
+            
+            series = data.get('data', {}).get('results', [])
+            valid_series = []
+
+            for serie in series:
+                thumbnail = serie.get('thumbnail', {})
+                if thumbnail.get('path') and thumbnail.get('extension') and 'image_not_available' not in thumbnail.get('path'):
+                    valid_series.append(serie)
+
+            context['series'] = valid_series
+
+        except requests.exceptions.RequestException as e:
+            context['error'] = f"Erreur lors de l'appel à l'API des séries : {str(e)}"
+            print(f"Erreur API: {e}")
+
+        except KeyError:
+            context['error'] = "Impossible de récupérer les données des séries."
 
         return context
