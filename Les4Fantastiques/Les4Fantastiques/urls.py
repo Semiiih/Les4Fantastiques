dbac07c06ad5011c30b01d8d35e20c16024e5210
Semih Cakici <semih.cakici34@gmail.com>
2024-12-10 14:45:22 +0100

    login , lougout , signup

--- Les4Fantastiques/Les4Fantastiques/urls.py
+++ Les4Fantastiques/Les4Fantastiques/urls.py
@@ -17,6 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import HomeView, PersonnageView, LoginView, SignupView, SerieView, FavoriView
+from django.shortcuts import redirect
+from django.views import View
+
+class CustomLogoutView(View):
+    def get(self, request):
+        from django.contrib.auth import logout
+        logout(request)
+        return redirect('home')
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
@@ -25,4 +33,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path('signup/', SignupView.as_view(), name='signup'),
     path("favori/", FavoriView.as_view(), name="favori"),
+    path('logout/', CustomLogoutView.as_view(), name='logout')
 ]
--- Les4Fantastiques/Les4Fantastiques/views/signup.py
+++ Les4Fantastiques/Les4Fantastiques/views/signup.py
@@ -1,10 +1,30 @@
-from django.views.generic import TemplateView
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+from django.views import View
+from django.contrib import messages
 
-class SignupView(TemplateView):
+# Vue pour l'inscription
+class SignupView(View):
     template_name = "signup.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["nom"] = "test"
-        return context
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password != confirm_password:
+            messages.error(request, "Les mots de passe ne correspondent pas.")
+            return render(request, self.template_name)
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_user(username=username, email=email, password=password)
+            messages.success(request, "Compte créé avec succès. Connectez-vous maintenant.")
+            return redirect('login')  
+        else:
+            messages.error(request, "Ce nom d'utilisateur existe déjà.")
+            return render(request, self.template_name)
